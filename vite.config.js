73ae68142800a99a957817decd82dfc13abd622e
Pooja <athanepooja313@gmail.com>
2026-02-26 19:33:10 +0530

    modifictaion in json

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => {
-  const isDev = command === 'serve'
-  const isVercel = process.env.VERCEL === '1'
-
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
+export default defineConfig({
+  plugins: [
+    react(),
+     tailwindcss()
     ],
-    base: isDev || isVercel ? '/' : '/Gmail_Clone/',
-  }
+  base: '/Gmail_Clone/',
 })
