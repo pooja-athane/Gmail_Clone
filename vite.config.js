82a52cd36ff041be1950286052de0ac46b751f9c
Pooja <athanepooja313@gmail.com>
2026-02-27 16:26:53 +0530

    in vite configuration changes the base path

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-     tailwindcss()
-    ],
-  base: '/Gmail_Clone/',
-})
+export default defineConfig(({ command }) => ({
+  plugins: [react(), tailwindcss()],
+  base: command === 'build' ? '/Gmail_Clone/' : '/',
+}))
