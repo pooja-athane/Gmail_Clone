9d7f12fc580a6238711e50ffdc7fa424ee41f18e
Pooja <athanepooja313@gmail.com>
2026-02-27 16:38:58 +0530

    adding deplyment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: command === 'build' ? '/Gmail_Clone/' : '/',
-}))
+  base: process.env.VERCEL ? '/' : '/Gmail_Clone/',
+})
